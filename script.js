67ca9d19bf45159ef2f7245de710d834e59109e4
chaitanyakonde <155949637+chaitanyakonde@users.noreply.github.com>
2024-03-11 19:30:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+{
+    "workbench.colorTheme": "Default Dark+",
+    "files.autoSave": "afterDelay",
+    "files.autoSaveDelay": 600,
+    "tabnine.experimentalAutoImports": true,
+    "liveServer.settings.donotShowInfoMsg": true,
+    "liveServer.settings.donotVerifyTags": true
+}
